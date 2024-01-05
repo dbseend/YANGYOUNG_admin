a5cb55e73be37f81a89c4acdbbc51398f3ae7c9e
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-01-05 20:26:07 +0900

    Kye (#7)
    * edited typo error
    
    * 기억안남
    
    * designed NavBar
    
    * 출결화면 구성

---
+++ src/components/WebAtTable.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+const AtTable = ({buttonText}) => {
+return (
+<div>
+    <div>{`${buttonText}반 출석부`}</div>
+    
+</div>
+);
+};
+
+export default AtTable;
