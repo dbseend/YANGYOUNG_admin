569664b99775a21631690fe0710859f81def0aa0
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-01-05 18:00:10 +0900

    출결화면 구성

---
+++ src/components/WebAtTable.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+const AtTable = ({buttonText}) => {
+return (
+<div>
+    <div>{`${buttonText}반 출석부`}</div>
+</div>
+);
+};
+
+export default AtTable;
--- src/components/WebAttendance.js
+++ src/components/WebAttendance.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
-import {useRecoilState} from "recoil";
-import { serialNumberState } from "../recoil/atom";
-import { checkTokenValidity } from "../api/AdminApi";
-import Navbar from "./WebNavbar";
+import { useRecoilState } from "recoil";
+import AtTable from "./WebAtTable";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -15,27 +13,64 @@ const GlobalStyle = createGlobalStyle`
 
 const Div = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   margin: 0;
   overflow: hidden;
 `;
 
 const Attendance = () => {
   const [date, setDate] = useState("");
-  const changeDate = (e) =>{
+  const changeDate = (e) => {
     setDate(e.target.value);
     console.log(e.target.value);
-  }
-    return (
-      <>
-        <GlobalStyle />
-        <Div>
-            <div>날짜를 선택하세요</div>
-            <input type = "date" onChange={changeDate}></input>
-            <div>{date}</div>
-        </Div>
-      </>
-    );
   };
-  export default Attendance;
-  
+  const [buttonText, setButtonText] = useState("");
+  const handleButtonClick = (buttonNumber) => {
+    // 각 버튼을 누를 때마다 다른 내용 설정
+    switch (buttonNumber) {
+      case 1:
+        setButtonText("W");
+        break;
+      case 2:
+        setButtonText("I");
+        break;
+      case 3:
+        setButtonText("N");
+        break;
+      case 4:
+        setButtonText("T");
+        break;
+      case 5:
+        setButtonText("E");
+        break;
+      case 6:
+        setButtonText("R");
+        break;
+      default:
+        setButtonText("");
+    }
+  };
+  return (
+    <>
+      <GlobalStyle />
+      <Div>
+        <div>날짜를 선택하세요</div>
+        <input type="date" onChange={changeDate}></input>
+        <div>{date}</div>
+        <div>
+          <button onClick={() => handleButtonClick(1)}>W</button>
+          <button onClick={() => handleButtonClick(2)}>I</button>
+          <button onClick={() => handleButtonClick(3)}>N</button>
+          <button onClick={() => handleButtonClick(4)}>T</button>
+          <button onClick={() => handleButtonClick(5)}>E</button>
+          <button onClick={() => handleButtonClick(6)}>R</button>
+          <div>
+            {/* 현재 보여지는 내용을 표시하는 부분 */}
+            {buttonText && <AtTable buttonText={buttonText} />}
+          </div>
+        </div>
+      </Div>
+    </>
+  );
+};
+export default Attendance;
