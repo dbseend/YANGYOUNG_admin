13b060a8451fe1ebde0be55051feace27d0ea29f
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-01-12 03:15:04 +0900

    충돌

--- src/api/StudentApi.js
+++ src/api/StudentApi.js
@@ -13,29 +13,6 @@ export const addStudent = async (id, name, school, grade, phoneNumber) => {
   try {
     const response = await axios.post(
       process.env.REACT_APP_URL + `/student`,
-      process.env.REACT_APP_URL + `/student`,
-      data
-    );
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-import axios from "axios";
-
-export const addStudent = async (id, name, school, grade, phoneNumber) => {
-  const data = {
-    id,
-    name,
-    school,
-    grade,
-    phoneNumber,
-  };
-
-  try {
-    const response = await axios.post(
-      process.env.REACT_APP_URL + `/student`,
       data
     );
     console.log(response.data);
