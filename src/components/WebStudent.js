4dddbb4dae117116710fd2e91418337ac8ab7404
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-01-12 02:09:46 +0900

    전체 학생 조회 구현

--- src/components/WebStudent.js
+++ src/components/WebStudent.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 import { useRecoilState } from "recoil";
-import { addStudent } from "../api/StudentAPI";
+import { addStudent, addStudentSection, viewStudent } from "../api/StudentAPI";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -17,67 +17,76 @@ const Div = styled.div`
   flex-direction: column;
   overflow: auto;
 `;
-const ButtonWrapper = styled.div`
-  margin-top: 20px;
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-`;
-const Button = styled.button`
-  cursor: pointer;
-  /* width: 30px;
-  height: 20px; */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+
 const Form = styled.form`
   display: flex;
   flex-direction: column;
 `;
+
+const TableContainer = styled.div`
+  margin: 20px;
+`;
+
+const StyledTable = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  margin-top: 20px;
+`;
+
+const StyledThead = styled.thead`
+  background-color: #f2f2f2;
+`;
+
+const StyledTh = styled.th`
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+`;
+
+const StyledTd = styled.td`
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+`;
+
+const HoverTr = styled.tr`
+  &:hover {
+    background-color: #f5f5f5;
+  }
+`;
+
 const Student = () => {
   const [id, setId] = useState("");
   const [name, setName] = useState("");
   const [school, setSchool] = useState("");
   const [grade, setGrade] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
-  const [buttonText, setButtonText] = useState("");
-  const handleButtonClick = (buttonNumber) => {
-
-    // 각 버튼을 누를 때마다 다른 내용 설정
-    switch (buttonNumber) {
-      case 1:
-        setButtonText("W");
-        break;
-      case 2:
-        setButtonText("I");
-        break;
-      case 3:
-        setButtonText("N");
-        break;
-      case 4:
-        setButtonText("T");
-        break;
-      case 5:
-        setButtonText("E");
-        break;
-      case 6:
-        setButtonText("R");
-        break;
-      default:
-        setButtonText("");
+  const [studentList, setStudentList] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [selectedBan, setSelectedBan] = useState("");
+  const [sectionId, setSectionId] = useState(0);
+  const ban = ["W", "I", "N", "T", "E", "R"];
+  const viewAllStudent = async () => {
+    try {
+      const { studentOneResponseList, count } = await viewStudent();
+      setStudentList(studentOneResponseList);
+      setTotalCount(count);
+    } catch (error) {
+      console.error("Error fetching student data:", error);
     }
   };
-
   const postStudent = (e) => {
     try {
       e.preventDefault();
       addStudent(id, name, school, grade, phoneNumber);
+      addStudentSection(id,sectionId);
       setId("");
       setName("");
       setSchool("");
       setGrade("");
       setPhoneNumber("");
+      setSelectedBan("");
+      setSectionId(0);
     } catch (error) {
       console.log(error);
     }
@@ -99,19 +108,36 @@ const Student = () => {
   const handleChangePhoneNumber = (e) => {
     setPhoneNumber(e.target.value); // 수정된 부분
   };
+  const handleDropdownChange = (e) => {
+    const selectedValue = e.target.value;
 
+    setSelectedBan(selectedValue);
+
+    switch (selectedValue) {
+      case "W":
+        setSectionId(1);
+        break;
+      case "I":
+        setSectionId(2);
+        break;
+      case "N":
+        setSectionId(3);
+        break;
+      case "T":
+        setSectionId(4);
+        break;
+      case "E":
+        setSectionId(5);
+        break;
+      case "R":
+        setSectionId(6);
+        break;
+    }
+  };
   return (
     <>
       <GlobalStyle />
       <Div>
-      <ButtonWrapper>
-          <Button onClick={() => handleButtonClick(1)}>학생 등록</Button>
-          <Button onClick={() => handleButtonClick(2)}>I</Button>
-          <Button onClick={() => handleButtonClick(3)}>N</Button>
-          <Button onClick={() => handleButtonClick(4)}>T</Button>
-          <Button onClick={() => handleButtonClick(5)}>E</Button>
-          <Button onClick={() => handleButtonClick(6)}>R</Button>
-        </ButtonWrapper>
         <Form onSubmit={postStudent}>
           id:
           <input type="text" value={id} onChange={handleChangeId} />
@@ -127,8 +153,45 @@ const Student = () => {
             value={phoneNumber}
             onChange={handleChangePhoneNumber}
           />
+          <select onChange={handleDropdownChange} value={selectedBan || ""}>
+            <option disabled value="">
+              반 선택
+            </option>
+            {ban.map((banOption) => (
+              <option key={banOption} value={banOption}>
+                {banOption}
+              </option>
+            ))}
+          </select>
           <button>학생 정보 등록</button>
         </Form>
+        <button onClick={viewAllStudent}>학생 정보 조회</button>
+        <TableContainer>
+          <h1>Student List</h1>
+          <p>등록된 전체 원생 수: {totalCount}</p>
+          <StyledTable>
+            <StyledThead>
+              <tr>
+                <StyledTh>ID</StyledTh>
+                <StyledTh>이름</StyledTh>
+                <StyledTh>학교</StyledTh>
+                <StyledTh>학년</StyledTh>
+                <StyledTh>연락처</StyledTh>
+              </tr>
+            </StyledThead>
+            <tbody>
+              {studentList.map((student) => (
+                <HoverTr key={student.id}>
+                  <StyledTd>{student.id}</StyledTd>
+                  <StyledTd>{student.name}</StyledTd>
+                  <StyledTd>{student.school}</StyledTd>
+                  <StyledTd>{student.grade}</StyledTd>
+                  <StyledTd>{student.phoneNumber}</StyledTd>
+                </HoverTr>
+              ))}
+            </tbody>
+          </StyledTable>
+        </TableContainer>
       </Div>
     </>
   );
