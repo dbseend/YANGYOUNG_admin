29439918d775b2a9d7fc6d105a8de171674619ab
dbseend <shyoon840@gmail.com>
2024-01-13 14:27:57 +0900

    출석 기능 완성

--- src/api/AttendanceApi.js
+++ src/api/AttendanceApi.js
@@ -19,3 +19,20 @@ export const getSectionAttendanceInfo = async (id, date) => {
     console.error("Error fetching attendance info:", error);
   }
 };
+
+export const postAttendanceBySection = async (data) => {
+  try {
+    const response = await axios.patch(
+      `${process.env.REACT_APP_URL}/attendance`,
+      data
+    );
+    console.log(response.data);
+
+    alert("출석 정보가 수정되었습니다");
+
+    return response.data;
+  } catch (error) {
+    alert("출석 정보가 수정 중 오류 발생했습니다");
+    console.error("Error fetching attendance info:", error);
+  }
+};
