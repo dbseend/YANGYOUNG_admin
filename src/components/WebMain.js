ed669c06f7bf589067090e7e0cdcb0c423f523e0
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-01-22 13:33:01 +0900

    Ynny (#34)
    * 기능 제한
    
    * 로그인 페이지 디자인

--- src/components/WebMain.js
+++ src/components/WebMain.js
@@ -6,7 +6,7 @@ import { useRecoilState } from "recoil";
 import { serialNumberState } from "../recoil/atom";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../fbase";
-// import { checkTokenValidity } from "../api/StudentApi";
+import Login from "../assets/LoginImage.svg";
 const GlobalStyle = createGlobalStyle`
   body {
     margin : 0;
@@ -16,87 +16,51 @@ const GlobalStyle = createGlobalStyle`
 
 const Div = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   margin: 0;
   overflow: auto;
-`;
-const Color = styled.div`
-  width: 50vw;
-  height: 100vh;
-  flex-shrink: 0;
-  background: #9D221A;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 `;
 
-const Texts = styled.div`
-  float: right;
-  margin-left: 146px;
-  margin-top: 150px;
-  flex-shrink: 0;
-  /* display: flex;
-  flex-direction: column;
-  justify-content: center; */
+const Container = styled.div`
+  margin-top: 133px;
 `;
-const Login = styled.div`
+const Head = styled.div`
+  justify-content: center;
+  margin-bottom: 40px;
+`;
+const Title = styled.div`
   color: #000;
-  font-family: IBM Plex Sans KR;
-  font-size: 40px;
+  font-family: Poppins;
+  font-size: 70px;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-  margin-left: 80px;
+  margin-bottom: 15px;
 `;
 
-const Content = styled.div`
+const SubT = styled.div`
   color: #000;
-  font-family: IBM Plex Sans KR;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 300;
-  line-height: normal;
-`;
-
-const Token = styled.div`
-  color: #000;
-  font-family: IBM Plex Sans KR;
+  font-family: Poppins;
   font-size: 30px;
   font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  margin-top: 70px;
-`;
-
-const Inputing = styled.input`
-  margin-top: 12px;
-  padding-left: 10px;
-  width: 331px;
-  height: 35px;
-  flex-shrink: 0;
-  stroke-width: 1px;
-  stroke: #b5b5b5;
-  font-family: IBM Plex Sans KR;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 500;
+  font-weight: 400;
   line-height: normal;
 `;
-
 const Button = styled.button`
-  margin-top: 30px;
-  display: flex;
-  width: 349px;
-  height: 45px;
-  padding: 10px;
-  justify-content: center;
-  align-items: center;
+  width: 287px;
+  height: 51.933px;
   flex-shrink: 0;
-  background: #2d6f4c;
+  border-radius: 6px;
+  background: #000;
   color: #fff;
-  font-family: IBM Plex Sans KR;
+  font-family: Poppins;
   font-size: 20px;
   font-style: normal;
   font-weight: 500;
   line-height: normal;
-  border-radius: 3px;
 `;
 const Main = () => {
   const navigate = useNavigate();
@@ -111,8 +75,8 @@ const Main = () => {
       console.log("유저 ", user);
 
       // if (user.email === "black.princeee@gmail.com") {
-        localStorage.setItem("email", user.email);
-        navigate("/attendance");
+      localStorage.setItem("email", user.email);
+      navigate("/attendance");
       // } else {
       //   alert("허용되지 않은 이메일입니다");
       // }
@@ -135,11 +99,14 @@ const Main = () => {
     <>
       <GlobalStyle />
       <Div>
-        <Color></Color>
-        <Texts>
-          <Login>양영학원</Login>
+        <Container>
+          <Head>
+            <Title>양영학원 고등부 영어과</Title>
+            <SubT>지정된 이메일로 로그인하세요.</SubT>
+          </Head>
           <Button onClick={handleLogin}>Login</Button>
-        </Texts>
+        </Container>
+        <img src={Login} />
       </Div>
     </>
   );
