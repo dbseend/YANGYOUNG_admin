1cfee23070a8830b922764224a69ea81e22dc18a
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-02-23 20:40:24 +0900

    Ynny (#57)
    * 기능 제한
    
    * 로그인 페이지 디자인
    
    * 넷바 디자인
    
    * 출결관리 디자인 하는 중
    
    * 출결 페이지 디자인, 일부 오류 수정
    
    * 출결 페이지 디자인. 학생 조회 컴포넌트 분리
    
    * 학생 조회 수정, 컴포넌트 분리
    
    * 뭔가했는데?
    
    * 학생 페이지 디자인
    
    * 학생 등록 기능 추가 중 500에러 발생
    
    * 학생 등록 기능 추가 중 500에러 발생 확인
    
    * 상세페이지
    
    * 수정 기능 구현 중. 415에러 나서 API에 app/json 명시
    
    * 삭제 기능 구현
    
    * 수정 Ing
    
    * .,수습
    
    * help me
    
    * 점검
    
    * 학생 추가 오류 수정
    
    * add - alert 수정
    
    * 디렉토리 정리, 넷바 글자 안 깨지도록 수정
    
    * 넷바 수정
    
    * 출결페이지 수정
    
    * ㅁㅝㄴ가수정함..
    
    * section 조회 기능 구현, api 추가
    
    * 강의 디테일 api
    
    * section 기능 추가
    
    * 분반 상세 페이지
    
    * 기능 수정
    
    * attendanceAPI수정중

--- src/components/Section/WebSection.js
+++ src/components/Section/WebSection.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 import { viewSection } from "../../api/SectionAPI";
