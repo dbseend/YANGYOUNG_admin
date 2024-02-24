208e30892788b03e8d182a196c73563af9c08f1f
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-02-25 00:51:16 +0900

    분반 삭제

--- src/components/Section/WebSection.js
+++ src/components/Section/WebSection.js
@@ -2,23 +2,31 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
-import { viewSection } from "../../api/SectionAPI";
+import { viewSection, deleteSection } from "../../api/SectionAPI";
 import AddSectionModal from "./AddSectionModal";
 import { Button } from "../Attendance/WebAtTable";
 
+const columns = [
+  { key: "index", label: "#" },
+  { key: "id", label: "ID" },
+  { key: "sectionName", label: "반 이름" },
+  { key: "teacher", label: "담임" },
+];
 
 const Section = () => {
   const [sectionList, setSectionList] = useState([]);
   const [sectionCount, setSectionCount] = useState(0);
-  const [selectedStudents, setSelectedStudents] = useState([]);
   const [isAddModalOpen, setAddModalOpen] = useState(false);
+  const [selectedSections, setSelectedSections] = useState([]);
+
   const navigate = useNavigate();
+
   const handleRowClick = (sectionId) => {
     moveToSectionDetail(sectionId);
   };
   const moveToSectionDetail = (sectionId) => {
     navigate(`/section/${sectionId}`);
-  }
+  };
   useEffect(() => {
     async function fetchData() {
       try {
@@ -32,6 +40,31 @@ const Section = () => {
     }
     fetchData();
   }, []);
+
+  const handleCheckboxChange = (sectionId) => {
+    // Toggle the selection state of the lecture
+    setSelectedSections((prevSelectedSections) => {
+      if (prevSelectedSections.includes(sectionId)) {
+        return prevSelectedSections.filter((id) => id !== sectionId);
+      } else {
+        return [...prevSelectedSections, sectionId];
+      }
+    });
+  };
+  const handleDeleteSelectedSections = async (sectionId) => {
+    try {
+      for (const secitonId of selectedSections) {
+        await deleteSection(sectionId);
+      }
+      setSelectedSections([]);
+      alert("선택한 학생이 삭제되었습니다.");
+      // window.location.reload(true);
+    } catch (error) {
+      console.error("분반 삭제 중 오류 발생", error);
+    }
+    deleteSection(sectionId);
+    console.log("Delete selected lectures:", selectedSections);
+  };
 
   const openAddModal = () => {
     setAddModalOpen(true);
@@ -48,33 +81,48 @@ const Section = () => {
     <>
       <GlobalStyle />
       <Div>
-        
         <TableContainer>
-        
           <h1>분반 정보</h1>
           <p> 개설 분반 수: {sectionCount}</p>
           <Button onClick={openAddModal}>등록</Button>
-        {isAddModalOpen && (
-          <AddSectionModal onClose={closeAddModal} onAdd={handleAddSection} />
-        )}
+          {isAddModalOpen && (
+            <AddSectionModal onClose={closeAddModal} onAdd={handleAddSection} />
+          )}
+          {/* 삭제 버튼 추가 */}
+          <Button onClick={handleDeleteSelectedSections}>
+            선택한 분반 삭제
+          </Button>
+
           <StyledTable>
-        <StyledThead>
-          <tr>
-            <StyledTh>ID</StyledTh>
-            <StyledTh>반 이름</StyledTh>
-            <StyledTh>담임</StyledTh>
-          </tr>
-        </StyledThead>
-        <tbody>
-          {sectionList.map((section) => (
-            <HoverTr key={section.id} onClick={() => handleRowClick(section.id)}>
-              <StyledTd>{section.id}</StyledTd>
-              <StyledTd>{section.name}</StyledTd>
-              <StyledTd>{section.teacher}</StyledTd>
-            </HoverTr>
-          ))}
-        </tbody>
-      </StyledTable>
+            <thead>
+              <tr>
+                {columns.map((column) => (
+                  <StyledTh key={column.key}>{column.label}</StyledTh>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {sectionList.map((section, index) => (
+                <StyledTr key={index}>
+                  {columns.map((column) => (
+                    <StyledTd
+                      onClick={() => moveToSectionDetail(section.id)}
+                      key={column.key}
+                    >
+                      {column.key === "index" ? index + 1 : section[column.key]}
+                    </StyledTd>
+                  ))}
+                  <StyledTd>
+                    <input
+                      type="checkbox"
+                      checked={selectedSections.includes(section.id)}
+                      onChange={() => handleCheckboxChange(section.id)}
+                    />
+                  </StyledTd>
+                </StyledTr>
+              ))}
+            </tbody>
+          </StyledTable>
         </TableContainer>
       </Div>
     </>
@@ -96,7 +144,7 @@ const Div = styled.div`
 `;
 
 const TableContainer = styled.div`
-margin: 20px;
+  margin: 20px;
 `;
 
 const StyledTable = styled.table`
@@ -131,5 +179,11 @@ const HoverTr = styled.tr`
     background-color: #f5f5f5;
   }
 `;
+const StyledTr = styled.tr`
+  cursor: pointer;
 
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`;
 export default Section;
--- src/components/Section/WebSectionDetail.js
+++ src/components/Section/WebSectionDetail.js
@@ -6,26 +6,14 @@ import { getOneSection } from "../../api/SectionAPI";
 const WebSectionDetail = () => {
   const { id } = useParams();
   const [sectionOneInfo, setSectionOneInfo] = useState([]);
-  const [sectionLectureCount, setSectionLectureCount] = useState(0);
-  const {sectionLectureInfo, setSectionLectureInfo} = useState({});
-  const [studentPersonalInfo, setStudentPersonalInfo] = useState({});
-  const [originalStudentPersonalInfo, setOriginalStudentPersonalInfo] =
-    useState({});
+  const [sectionLectureCount] = useState(0);
   const [lectureInfo, setLectureInfo] = useState([]);
-  const [lectureCount, setLectureCount] = useState(0);
-  const [taskInfo, setTaskInfo] = useState([]);
-  const [taskCount, setTaskCount] = useState(0);
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [selectedSection, setSelectedSection] = useState(0);
-  const [selectedGrade, setSelectedGrade] = useState("");
-  const [grades, setGrades] = useState(["중3", "고1", "고2", "고3"]);
-  const [sections, setSections] = useState(["A","I","N","T","E","R"]); // API로부터 섹션 정보를 가져와야 함
 
   useEffect(() => {
     const fetchStudentDetail = async () => {
       try {
         const response = await getOneSection(id);
-        console.log (response.data);
+        console.log(response.data);
         setSectionOneInfo(response.data.sectionResponse);
         // setSectionLectureInfo(response.data.lectureGetAllResponse);
       } catch (error) {
@@ -35,112 +23,28 @@ const WebSectionDetail = () => {
     fetchStudentDetail();
   }, [id]);
 
-  const handleToggleEditMode = () => {
-    setIsEditMode(!isEditMode);
-    // 수정 모드가 종료되면 원래의 정보로 복원
-    if (!isEditMode) {
-      setStudentPersonalInfo(originalStudentPersonalInfo);
-    }
-  };
-
-//   const handleSaveChanges = async () => {
-//     try {
-//       console.log("수정 전 학생 정보:", studentPersonalInfo);
-//       await editStudentInfo({
-//         studentId: studentPersonalInfo.id,
-//         school: studentPersonalInfo.school,
-//         grade: "M3",
-//         phoneNumber: studentPersonalInfo.phoneNumber,
-//         sectionId: parseInt(selectedSection),
-//       });
-//       // 학생 정보가 성공적으로 업데이트되면 편집 모드를 해제합니다.
-//       setIsEditMode(false);
-//       console.log("수정 후 학생 정보:", studentPersonalInfo);
-//     } catch (error) {
-//       console.error("학생 정보 수정 중 오류 발생: ", error);
-//       // 에러 처리
-//     }
-//   };
-  
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setStudentPersonalInfo({
-      ...studentPersonalInfo,
-      [name]: value,
-    });
-  };
-
-  const handleSectionChange = (e) => {
-    setSelectedSection(e.target.value);
-  };
-
-  const handleGradeChange = (e) => {
-    setSelectedGrade(e.target.value);
-  };
-
   return (
     <Div>
       <Title>상세 정보</Title>
-      <Container>
-      <Button onClick={handleToggleEditMode}>
-        {isEditMode ? "취소" : "수정"}
-      </Button>
-      {isEditMode && (
-        <Button>저장</Button>
-        // <Button onClick={handleSaveChanges}>저장</Button>
-
-      )}
-      </Container>
       <Guide1>분반 정보</Guide1>
       <Table>
         <tbody>
           <tr>
-          <th>분반명</th>
-            <td>
-              {isEditMode ? (
-                <input
-                  type="text"
-                  name="name"
-                  value={sectionOneInfo.name}
-                  onChange={handleInputChange}
-                />
-              ) : (
-                sectionOneInfo.name
-              )}
-            </td>
+            <th>분반명</th>
+            <td>{sectionOneInfo.name}</td>
             <th>id</th>
-            <td>
-              {isEditMode ? (
-                <input
-                  type="text"
-                  name="id"
-                  value={sectionOneInfo.id}
-                  onChange={handleInputChange}
-                />
-              ) : (
-                sectionOneInfo.id
-              )}
-            </td>
+            <td>{sectionOneInfo.id}</td>
             <th>담임</th>
-            <td>
-              {isEditMode ? (
-                <input
-                  type="text"
-                  name="id"
-                  value={sectionOneInfo.teacher}
-                  onChange={handleInputChange}
-                />
-              ) : (
-                sectionOneInfo.teacher
-              )}
-            </td>
+            <td>{sectionOneInfo.teacher}</td>
           </tr>
         </tbody>
       </Table>
 
       <Guide2>수강 정보</Guide2>
-      <p>{sectionOneInfo.name} 분반에는 총 {sectionLectureCount}개의 수업이 배정되어 있습니다.</p>
+      <p>
+        {sectionOneInfo.name} 분반에는 총 {sectionLectureCount}개의 수업이
+        배정되어 있습니다.
+      </p>
       <Table>
         <thead>
           <tr>
@@ -161,7 +65,6 @@ const WebSectionDetail = () => {
           ))}
         </tbody>
       </Table>
-
     </Div>
   );
 };
@@ -176,11 +79,6 @@ const Div = styled.div`
   margin-right: 12.5%;
 `;
 
-const Container = styled.div`
-display: flex;
-flex-direction: row;
-gap: 23px;
-`
 const Title = styled.div`
   color: #000;
   font-family: Poppins;
@@ -214,21 +112,6 @@ const Table = styled.table`
     border: 1px solid #ddd;
     text-align: center;
   }
-`;
-
-const Button = styled.button`
-  cursor: pointer;
-  width: 80px;
-  height: 30px;
-  border-radius: 6px;
-  background: #000;
-  color: #fff;
-  font-family: Poppins;
-  font-size: 15px;
-  font-style: normal;
-  line-height: normal;
-  border: none;
-  margin-right: 10px;
 `;
 
 export default WebSectionDetail;
