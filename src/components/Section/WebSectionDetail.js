4cc7e7ec84021971864269695cb938c0a49be102
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-02-25 04:35:50 +0900

    분반 상세 페이지

--- src/components/Section/WebSectionDetail.js
+++ src/components/Section/WebSectionDetail.js
@@ -3,21 +3,43 @@ import styled from "styled-components";
 import { useParams } from "react-router-dom";
 import { getOneSection } from "../../api/SectionAPI";
 
+const lectures = [
+  { key: "index", label: "순번" },
+  { key: "name", label: "강의명" },
+  { key: "day", label: "요일" },
+  { key: "time", label: "시간" },
+  { key: "room", label: "강의실" },
+];
+
 const WebSectionDetail = () => {
   const { id } = useParams();
-  const [sectionOneInfo, setSectionOneInfo] = useState([]);
-  const [sectionLectureCount] = useState(0);
-  const [lectureInfo, setLectureInfo] = useState([]);
+  const [sectionOneInfo, setSectionOneInfo] = useState({});
+  const [sectionLectureInfo, setSectionLectureInfo] = useState({
+    lectureGetAllResponse: [],
+    count: 0,
+  });
 
   useEffect(() => {
     const fetchStudentDetail = async () => {
       try {
         const response = await getOneSection(id);
         console.log(response.data);
+
+        // 첫 번째 상태 업데이트
         setSectionOneInfo(response.data.sectionResponse);
-        // setSectionLectureInfo(response.data.lectureGetAllResponse);
+        console.log(
+          "sectionOneInfo after update:",
+          response.data.sectionResponse
+        );
+
+        // 두 번째 상태 업데이트
+        setSectionLectureInfo(response.data.lectureAllResponse);
+        console.log(
+          "sectionLectureInfo after update:",
+          response.data.lectureAllResponse
+        );
       } catch (error) {
-        console.log("학생 상세 정보 가져오는 중 오류 발생: ", error);
+        console.log("분반 상세 정보 가져오는 중 오류 발생: ", error);
       }
     };
     fetchStudentDetail();
@@ -42,27 +64,28 @@ const WebSectionDetail = () => {
 
       <Guide2>수강 정보</Guide2>
       <p>
-        {sectionOneInfo.name} 분반에는 총 {sectionLectureCount}개의 수업이
+        {sectionOneInfo.name} 분반에는 총 {sectionLectureInfo.count}개의 수업이
         배정되어 있습니다.
       </p>
       <Table>
         <thead>
           <tr>
-            <th>강의명</th>
-            <th>요일</th>
-            <th>시간</th>
-            <th>강의실</th>
+            {lectures.map((lecture) => (
+              <th key={lecture.key}>{lecture.label}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {lectureInfo.map((lecture) => (
-            <tr key={lecture.id}>
-              <td>{lecture.name}</td>
-              <td>{lecture.day}</td>
-              <td>{lecture.time}</td>
-              <td>{lecture.room}</td>
-            </tr>
-          ))}
+          {sectionLectureInfo.lectureResponseList &&
+            sectionLectureInfo.lectureResponseList.map((lecture, index) => (
+              <tr key={index}>
+                {lectures.map((col) => (
+                  <td key={col.key}>
+                    {col.key === "index" ? index + 1 : lecture[col.key]}
+                  </td>
+                ))}
+              </tr>
+            ))}
         </tbody>
       </Table>
     </Div>
