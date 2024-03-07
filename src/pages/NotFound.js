510f0cf7ece1ec0422636a06fd65c88f1526333e
dbseend <shyoon840@gmail.com>
2024-03-08 00:06:20 +0900

    [FEAT]: 404 페이지 생성

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>매일매일 성장하는 개발자들</h1>
+      <h2>한동대학교 22학번 웹 개발 왕 김예은</h2>
+      <h2>왕 밑에서 열심히 배우는 윤성현</h2>
+    </div>
+  );
+};
+
+export default NotFound;
