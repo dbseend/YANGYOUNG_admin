a1b23d1cb9e3484643772a77660164e450a1ee14
dbseend <shyoon840@gmail.com>
2024-03-24 10:17:30 +0900

    [FIX] : 멤버 할당 API 수정

--- src/api/SectionAPI.js
+++ src/api/SectionAPI.js
@@ -54,9 +54,9 @@ export const updateSectionAPI = async (updateData) => {
   console.log(updateData);
   try {
     const response = await axios.patch(
-      process.env.REACT_APP_URL + `section`,
+      process.env.REACT_APP_URL + `section/student`,
       // process.env.REACT_APP_DEV_URL + `section`,
-      // 'http://localhost:8080/api/v0/section',
+      // 'http://localhost:8080/api/v0/section/student',
       updateData
     );
     return response;
